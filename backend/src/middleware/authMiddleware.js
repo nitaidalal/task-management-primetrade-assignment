08a43c674edfc184f3f5609258841c06ceec67a3
nitaidalal <dalalnitai7@gmail.com>
2026-06-10 23:40:42 +0530

    Add authmiddleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, no token",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id)
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User no longer exists",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Token expired, please login again",
+      });
+    }
+    return res.status(401).json({
+      success: false,
+      message: "Not authorized, invalid token",
+    });
+  }
+};
